0f61a625ae26c49f69b461f5d51728971b8215a3
Erlend Åmdal <erlend@aamdal.com>
2018-05-21 20:01:30 +0200

    Use states instead of items for showable predicate
    We are scanning blocks, therefore it makes more sense to test the blocks
    instead of items. Also saves us constructing a ton of ItemStacks.

--- src/main/java/com/lothrazar/prospectors/ItemProspector.java
+++ src/main/java/com/lothrazar/prospectors/ItemProspector.java
@@ -1,6 +1,5 @@
 package com.lothrazar.prospectors;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import javax.annotation.Nonnull;
 import com.lothrazar.prospectors.Prospectors.Types;
 import net.minecraft.block.Block;
@@ -26,7 +25,7 @@ public class ItemProspector extends Item {
   public int cooldown;
   public int range;
   public boolean isBlacklist;
-  public String[] blocklist;
+  private Set<String> blockSet;
   public Types type;
   public ItemProspector(Types t) {
     super();
@@ -36,25 +35,21 @@ public class ItemProspector extends Item {
   @Override
   public EnumActionResult onItemUse(EntityPlayer player, World worldObj, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
     ItemStack stack = player.getHeldItem(hand);
-    if (side == null || pos == null) { return super.onItemUse(player, worldObj, pos, hand, side, hitX, hitY, hitZ); }
     Map<String, Integer> mapList = new HashMap<String, Integer>();
     String name;
     EnumFacing direction = side.getOpposite();
     BlockPos current;
-    IBlockState at;
+    IBlockState state;
     Block blockAt;
     ItemStack s;
     for (int i = 0; i < range; i++) {
       current = pos.offset(direction, i);
-      at = worldObj.getBlockState(current);
-      if (at == Blocks.AIR.getDefaultState()) {
+      state = worldObj.getBlockState(current);
+      if (state == Blocks.AIR.getDefaultState() || !isBlockShowable(state)) {
         continue;
       }
-      blockAt = at.getBlock();
-      s = new ItemStack(Item.getItemFromBlock(blockAt), 1, blockAt.getMetaFromState(at));
-      if (isBlockShowable(s) == false) {
-        continue;
-      }
+      blockAt = state.getBlock();
+      s = new ItemStack(Item.getItemFromBlock(blockAt), 1, blockAt.getMetaFromState(state));
       name = s.getDisplayName();
       int previous = (mapList.containsKey(name)) ? mapList.get(name) : 0;
       mapList.put(name, previous + 1);
@@ -85,34 +80,10 @@ public class ItemProspector extends Item {
   private static void addChatMessage(EntityPlayer player, String text) {
     player.sendMessage(new TextComponentTranslation(lang(text)));
   }
-  private boolean isBlockShowable(ItemStack stack) {
-    if (stack == null || stack.getItem() == null) { return false; } //nulls
-    String itemName = getStringForItemStack(stack);//this one includes metadata
-    String itemSimpleName = getStringForItem(stack.getItem());//this one doesnt
-    boolean isInList = false;
-    for (String s : blocklist) {//dont use .contains on the list. must use .equals on string
-      if (s == null) {
-        continue;
-      } //lol
-      //so if list has only "minecraft:stone" then all metadata is covered
-      //otherwise, list might have "minecraft:stone/3" so it only matches that
-      if (s.equals(itemName) || s.equals(itemSimpleName)) {
-        isInList = true;
-        break;
-      }
-    }
-    //if its a blacklist, and its IN the list, DONT show it (false)
-    //otherwise, its a whitelist, so if it IS in the list then show it (true)v
-    boolean yesShowIt = (this.isBlacklist) ? (!isInList) : isInList;
-    return yesShowIt;
-  }
-  private static @Nonnull String getStringForItemStack(ItemStack itemStack) {
-    Item item = itemStack.getItem();
-    return item.getRegistryName().getResourceDomain() + ":" + item.getRegistryName().getResourcePath() + "/" + itemStack.getMetadata();
-  }
-  private static String getStringForItem(Item item) {
-    if (item == null || item.getRegistryName() == null) { return ""; }
-    return item.getRegistryName().getResourceDomain() + ":" + item.getRegistryName().getResourcePath();
+  private boolean isBlockShowable(IBlockState state) {
+    String simpleName = state.getBlock().getRegistryName().toString();
+    String nameWithMeta = simpleName + "/" + state.getBlock().getMetaFromState(state);
+    return isBlacklist ^ (blockSet.contains(simpleName) || blockSet.contains(nameWithMeta));
   }
   public void syncConfig(Configuration c, String[] deflist) {
     String category = "prospector_" + this.type.name().toLowerCase();
@@ -120,7 +91,8 @@ public class ItemProspector extends Item {
     this.cooldown = c.getInt("cooldown", category, 10, 0, 256, "Time delay per use (ticks); zero to disable");
     this.setMaxDamage(c.getInt("durability", category, 200, 1, 65536, "Durability: number of uses"));
     this.isBlacklist = c.getBoolean("IsBlacklist", category, false, "True means this is a blacklist: ignore whats listed. False means its a whitelist: only print whats listed.");
-    this.blocklist = c.getStringList("ProspectorBlockList", category, deflist, "List of blocks that the Prospector knows about.");
+    String[] blockList = c.getStringList("ProspectorBlockList", category, deflist, "List of blocks that the Prospector knows about.");
+    this.blockSet = new HashSet<>(Arrays.asList(blockList));
   }
   public IRecipe addRecipe(ResourceLocation rl) {
     IRecipe recipe = null;
